d89ea596eafd902b54b6b0fdfb6bd697cf209ea0
Derek <dbdaniel@mtu.edu>
2015-01-17 12:03:13 -0500

    Updating

--- CaliWindow.py
+++ CaliWindow.py
@@ -12,6 +12,7 @@ logger = logging.getLogger('cali')
 from cali_lib import Window
 from cali.AboutCaliDialog import AboutCaliDialog
 from cali.PreferencesCaliDialog import PreferencesCaliDialog
+import sync as capi
 
 # See cali_lib.Window.py for more details about how this class works
 class CaliWindow(Window):
@@ -20,14 +21,23 @@ class CaliWindow(Window):
     def finish_initializing(self, builder): # pylint: disable=E1002
         """Set up the main window"""
         super(CaliWindow, self).finish_initializing(builder)
+        # action_group = Gtk.ActionGroup("my_actions")
+        callenderTarget=capi.CalenderAPI()
+        capi.CalenderAPI.get_events(callenderTarget)
 
         self.AboutDialog = AboutCaliDialog
         self.PreferencesDialog = PreferencesCaliDialog
 
         self.calendar = self.builder.get_object('calendar')
-        self.weekly_listbox = self.builder.get_object('')
+        self.weekly_listbox = self.builder.get_object('weekly_listbox')
+        # self.add_sync_menu_actions(action_group)
+        # self.weekly_listbox.insert('test',-1)
 
+        self.sync = self.builder.get_object('btnSync')
         # Code for other initialization actions should be added here.
 
     def on_calendar_day_selected(self,widget):
         print(widget.get_date())
+
+    def on_sync_clicked(self,widget):
+         print("You clicked sync")
---
+++ sync.py
@@ -0,0 +1,49 @@
+import gflags
+import httplib2
+import google_api_secrets as gas
+
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.tools import run
+
+
+
+
+
+class CalenderAPI():
+
+    def __init__(self):
+        global service,page_token
+        print("called")
+        self.FLAGS = gflags.FLAGS
+
+        self.FLOW = OAuth2WebServerFlow(
+            client_id=gas.api_key(),
+            client_secret=gas.client_secret(),
+            scope='https://www.googleapis.com/auth/calendar',
+            user_agent='cali/alpha 1')
+
+        self.storage = Storage('calendar.dat')
+        self.credentials = self.storage.get()
+        if self.credentials is None or self.credentials.invalid == True:
+          self.credentials = run(self.FLOW, self.storage)
+
+
+        self.http = httplib2.Http()
+        self.http = self.credentials.authorize(self.http)
+
+        service = build(serviceName='calendar', version='v3', http=self.http,
+            developerKey=gas.dev_key())
+
+
+
+    def get_events(self):
+         page_token = None
+         while True:
+          events = service.events().list(calendarId='primary', pageToken=page_token).execute()
+          for event in events['items']:
+            print(event)
+          page_token = events.get('nextPageToken')
+          if not page_token:
+            break
